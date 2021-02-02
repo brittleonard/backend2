483f0f8c543e30d1ef2a307b8e1da83c88513f3c
brittanyleonard <Belah615@gmail.com>
2021-02-02 13:43:15 -0600

    testing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   # skip_before_action :verify_authenticity_token
 
   def index
-      render json: Recipe.all
+      render json: { message: 'hi', status: 200 }
   end
 
 end
